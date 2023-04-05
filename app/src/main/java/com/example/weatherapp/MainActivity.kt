52fbc946d634047581783e507a13d1497065bba5
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-04-06 00:34:53 +1000

    Added textChanged function

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -12,8 +14,8 @@ import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
-    var baseCurrency = "AUD"
-    var convertedToCurrency = "USD"
+    private var baseCurrency = "AUD"
+    private var convertedToCurrency = "USD"
     var conversionRate = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +25,32 @@ class MainActivity : AppCompatActivity() {
         spinnerSetup()
         textChanged()
 
+    }
+
+    private fun textChanged() {
+        val firstConversion: EditText = findViewById(R.id.et_firstConversion)
+        val secondConversion: EditText = findViewById(R.id.et_secondConversion)
+
+        firstConversion.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Get the amount entered by the user
+                val amount = s.toString().toFloatOrNull() ?: 0f
+
+                // Calculate the conversion rate
+                val rate = amount * conversionRate
+
+                // Set the result in the second conversion EditText
+                secondConversion.setText(rate.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Do nothing
+            }
+        })
     }
 
 
@@ -47,10 +75,10 @@ class MainActivity : AppCompatActivity() {
 
             // Extract the response body as a string and convert it to a JSONObject
             val responseString = response.body()?.string()
-            val jsonObject = JSONObject(responseString)
+            val jsonObject = responseString?.let { JSONObject(it) }
 
             // Get the converted amount from the response and set it as the text of the output EditText view
-            val convertedAmount = jsonObject.getDouble("result")
+            val convertedAmount = jsonObject?.getDouble("result")
             secondConversion.setText(convertedAmount.toString())
         } else {
             // If the input amount is empty or blank, clear the output EditText view
